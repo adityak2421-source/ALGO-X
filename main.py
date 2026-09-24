e2eac4bf20dd74e10278440a89da9e049d60e58d
Aditya Kumar <adityak2421@gmail.com>
2026-09-24 23:33:12 +0530

    Initial ALGO-X project setup

---
+++ main.py
@@ -0,0 +1,7 @@
+print("========================================")
+print("                ALGO-X")
+print("     Interactive Algorithm Toolkit")
+print("========================================")
+
+print()
+print("ALGO-X is running successfully!")
